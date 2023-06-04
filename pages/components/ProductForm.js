9f65747452cc477cc9f8490b1cdd1fa7d30c915a
jjisolo24 <jjisolo24@gmail.com>
2023-06-04 16:33:03 +0300

    add missing files

--- pages/components/ProductForm.js
+++ pages/components/ProductForm.js
@@ -1,10 +1,13 @@
+import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
 export default function ProductForm({
+    _id,
     title:       existingTitle,
     description: existingDescription,
     price:       existingPrice,
+    images,
 }) {
     const [title,       setTitle]       = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
@@ -12,10 +15,16 @@ export default function ProductForm({
     const [goToProducts,setToToProducts]= useState(false);
     const router                        = useRouter();
     
-    async function createProduct(ev) {
+    async function saveProduct(ev) {
         ev.preventDefault();
         const data = {title, description, price};
-        await axios.post('/api/products', data);
+
+        if(_id) {
+            await axios.put('/api/products', {...data, _id});
+        } else {
+            await axios.post('/api/products', data);
+        }
+
         setToToProducts(true);
     }
 
@@ -23,8 +32,27 @@ export default function ProductForm({
         router.push('/products');
     }
 
+    async function uploadImages(ev) {
+        const files = ev.target?.files;
+
+        if(files?.length > 0) {
+            const data = new FormData();
+
+            for(const file of files) {
+                data.append("file", file);
+            }
+            
+            const res = await fetch('/api/upload', {
+                method: 'POST',
+                body: data,
+            });
+            
+            console.log(res);
+        }
+    }
+
     return(
-            <form onSubmit={createProduct}>
+            <form onSubmit={saveProduct}>
                 <div className="flex-col">
                     <label>Product Name</label>
                     <input type    ="text" 
@@ -32,6 +60,28 @@ export default function ProductForm({
                         value={title}
                         onChange={ev => setTitle(ev.target.value)}/>
                     
+                    <label>
+                        Images
+                    </label>
+                    <div className="mb-2">
+                        <label className="w-24 h-24 cursor-pointer text-center flex items-center justify-center textr-sm gap-1 text-gray-600 rounded-lg bg-gray-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                            </svg>
+
+                            <div>
+                                Upload
+                            </div>
+
+                            <input type='file' onChange={uploadImages} className="hidden" />
+                        </label>
+
+                        {!images?.length && (
+                            <div>No photos were uploaded</div>
+                        )}
+                    </div>
+                    
+
                     <label>Product Description</label>
                     <textarea placeholder="description"
                             value      ={description}
