9c79487b2e7a82605dc3f28e4d23aeb205727b26
jjisolo24 <jjisolo24@gmail.com>
2023-06-04 14:56:04 +0300

    api for products

---
+++ pages/products/edit/[...editProduct].js
@@ -0,0 +1,10 @@
+import Layout from "@/pages/components/Layout";
+
+export default function EditProductPage() {
+
+    return(
+        <Layout>
+            Edit product
+        </Layout>
+    )
+}
