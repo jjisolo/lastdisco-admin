700f5adccfc7f95e72fcd738c9b5bc541caa406e
jjisolo24 <jjisolo24@gmail.com>
2023-06-05 18:24:50 +0300

    add tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,12 +8,37 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily : {
+        sans : ['Barlow', 'sans-serif']
       },
+      colors : {
+        gray : {
+          100: '#E0E6E9',
+          500: '#ABBBC2',
+          700: '#393C49',
+          900: '#1F1D2B',
+          800: '#252836',
+        },
+        primary : '#EB966A',
+        accent : {
+          blue: '#65B0F6',
+          orange: '#FFB572',
+          red: '#FF7CA3',
+          green: '#50D1AA',
+          purple : '#9290FE'
+        }
+      },
+      boxShadow : {
+        primary : '0px 8px 24px rgba(234, 124, 105, 0.32)',
+        'inverse-top' : '4px 4px 0 #252836',
+        'inverse-bottom' : '4px -4px 0 #252836'
+      }
     },
   },
+  
+  variants: {
+    extend: {},
+  },
+
   plugins: [],
 }
