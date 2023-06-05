fe83fb2980e2bed0e54339dcc3240a868fdcff15
jjisolo24 <jjisolo24@gmail.com>
2023-06-05 06:21:55 +0300

    implement categories

--- pages/components/ProductForm.js
+++ pages/components/ProductForm.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useRouter } from "next/router";
-import { useState }  from "react";
+import { useEffect, useState }  from "react";
 import Spinner from "./Spinner";
 import { ReactSortable } from "react-sortablejs";
 
@@ -10,18 +10,27 @@ export default function ProductForm({
     description: existingDescription,
     price:       existingPrice,
     images:      existingImages,
+    category:    existingCategory
 }) {
-    const [title,       setTitle]       = useState(existingTitle || '');
+    const [category,    setCategory]    = useState(existingCategory    || '');
+    const [title,       setTitle]       = useState(existingTitle       || '');
     const [description, setDescription] = useState(existingDescription || '');
-    const [price,       setPrice]       = useState(existingPrice || '');
-    const [images,      setImages]      = useState(existingImages || []);
+    const [price,       setPrice]       = useState(existingPrice       || '');
+    const [images,      setImages]      = useState(existingImages      || []);
     const [goToProducts,setToToProducts]= useState(false);
     const [isUploading, setIsUploading] = useState(false);
+    const [categories,  setCategories ] = useState([]);
     const router                        = useRouter();
     
+    useEffect(() => {
+        axios.get('/api/categories').then(result => {
+            setCategories(result.data);
+        });
+    }, []);
+
     async function saveProduct(ev) {
         ev.preventDefault();
-        const data = {title, description, price, images};
+        const data = {title, description, price, images, category};
 
         if(_id) {
             await axios.put('/api/products', {...data, _id});
@@ -69,6 +78,17 @@ export default function ProductForm({
                         value={title}
                         onChange={ev => setTitle(ev.target.value)}/>
                     
+                    <label>Category</label>
+                    <select value={category} onChange={ev => setCategory(ev.target.value)}>
+                        <option value="">Uncategorized</option>
+                        
+                        {categories.length > 0 && categories.map(c => (
+                            <option value={c._id}>
+                                {c.name}
+                            </option>
+                        ))}
+                    </select>
+
                     <label>
                         Images
                     </label>
@@ -86,7 +106,6 @@ export default function ProductForm({
                             </div>
                         )}
                         
-
                         <label className="w-24 h-24 cursor-pointer text-center flex items-center justify-center textr-sm gap-1 text-gray-600 rounded-lg bg-gray-200">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
@@ -98,10 +117,6 @@ export default function ProductForm({
 
                             <input type='file' onChange={uploadImages} className="hidden" />
                         </label>
-
-                        {!images?.length && (
-                            <div>No photos were uploaded</div>
-                        )}
                     </div>
                     
 
