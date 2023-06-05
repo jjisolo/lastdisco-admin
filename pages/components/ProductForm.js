4ff97c28e676765121f1f2e6d6ee79e4f3a4cf8f
jjisolo24 <jjisolo24@gmail.com>
2023-06-05 04:49:55 +0300

    add image loading, sorting, spinners when loading

--- pages/components/ProductForm.js
+++ pages/components/ProductForm.js
@@ -1,23 +1,27 @@
 import axios from "axios";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState }  from "react";
+import Spinner from "./Spinner";
+import { ReactSortable } from "react-sortablejs";
 
 export default function ProductForm({
     _id,
     title:       existingTitle,
     description: existingDescription,
     price:       existingPrice,
-    images,
+    images:      existingImages,
 }) {
     const [title,       setTitle]       = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
     const [price,       setPrice]       = useState(existingPrice || '');
+    const [images,      setImages]      = useState(existingImages || []);
     const [goToProducts,setToToProducts]= useState(false);
+    const [isUploading, setIsUploading] = useState(false);
     const router                        = useRouter();
     
     async function saveProduct(ev) {
         ev.preventDefault();
-        const data = {title, description, price};
+        const data = {title, description, price, images};
 
         if(_id) {
             await axios.put('/api/products', {...data, _id});
@@ -36,21 +40,24 @@ export default function ProductForm({
         const files = ev.target?.files;
 
         if(files?.length > 0) {
-            const data = new FormData();
+            setIsUploading(true);
 
+            const data = new FormData();
             for(const file of files) {
                 data.append("file", file);
             }
             
-            //const res = await axios.post('/api/upload', {data});
-            const res = await fetch('/api/upload', {
-                method: 'POST',
-                body:   data,
+            const res = await axios.post('/api/upload', data);
+            setImages(oldImages => {
+                return [...oldImages, ...res.data.links];
             });
 
-            console.log("test");
-            console.log(res);
+            setIsUploading(false);
         }
+    }
+
+    function updateImagesOrder(images) {
+        setImages(images);
     }
 
     return(
@@ -65,7 +72,21 @@ export default function ProductForm({
                     <label>
                         Images
                     </label>
-                    <div className="mb-2">
+                    <div className="mb-2 flex flex-wrap gap-1">
+                        <ReactSortable list={images} className="flex flex-wrap gap-1" setList={updateImagesOrder}>
+                            {!!images?.length && images.map(link => (
+                                <div key={link} className="h-24">
+                                    <img src={link} alt="" className="rounded-lg"/>
+                                </div>
+                            ))}
+                        </ReactSortable>
+                        {isUploading && (
+                            <div className="h-24 p-1 flex items-center rounded-lg">
+                                <Spinner />
+                            </div>
+                        )}
+                        
+
                         <label className="w-24 h-24 cursor-pointer text-center flex items-center justify-center textr-sm gap-1 text-gray-600 rounded-lg bg-gray-200">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
---
+++ pages/components/Spinner.js
@@ -0,0 +1,7 @@
+import {SyncLoader} from "react-spinners";
+
+export default function Spinner() {
+    return (
+        <SyncLoader color={'#1e3a8a'} speedMultiplier={2} />
+    )
+}
